0d9be0249816e7d8b4c951f305b9d4c5a4a74f71
Kushen De Silva <kushenthimira@gmail.com>
2022-02-14 15:12:13 +0530

    v0.2.3
    -added required permissions & packages

--- src/screens/ChangeLocation.js
+++ src/screens/ChangeLocation.js
@@ -42,3 +42,58 @@ export default function ({ navigation }) {
     </Screen>
   );
 }
+
+// import React, { useState, useEffect } from 'react';
+// import { Platform, Text, View, StyleSheet } from 'react-native';
+// import Constants from 'expo-constants';
+// import * as Location from 'expo-location';
+
+// export default function App() {
+//   const [location, setLocation] = useState(null);
+//   const [errorMsg, setErrorMsg] = useState(null);
+
+//   useEffect(() => {
+//     (async () => {
+//       if (Platform.OS === 'android' && !Constants.isDevice) {
+//         setErrorMsg(
+//           'Oops, this will not work on Snack in an Android emulator. Try it on your device!'
+//         );
+//         return;
+//       }
+//       let { status } = await Location.requestForegroundPermissionsAsync();
+//       if (status !== 'granted') {
+//         setErrorMsg('Permission to access location was denied');
+//         return;
+//       }
+
+//       let location = await Location.getCurrentPositionAsync({});
+//       setLocation(location);
+//     })();
+//   }, []);
+
+//   let text = 'Waiting..';
+//   if (errorMsg) {
+//     text = errorMsg;
+//   } else if (location) {
+//     text = JSON.stringify(location);
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.paragraph}>{text}</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     padding: 20,
+//   },
+//   paragraph: {
+//     fontSize: 18,
+//     textAlign: 'center',
+//   },
+// });
